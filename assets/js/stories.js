9e7119f9b966776885fef39ba0e1ef6377f8debf
liturgy97 <liturgy97@gmail.com>
2024-07-09 21:10:17 +0300

    reset

---
+++ assets/js/stories.js
@@ -0,0 +1,294 @@
+// data.stories[""] = {
+//   num: ,
+//   folder: "",
+//   overview: "",
+//   severity: "D",
+//   NTR: "None",
+//   "duration": 526,
+//   genre: ["", ],
+//   characters: ["", ],
+//   chapters: {
+//       1: {
+//           name: "",
+//           "duration": ,
+//           isPatreon: false,
+//           isComingSoon: false,
+//           "currentPassage": "",
+//           "passageNames": [""]
+//       }, 
+//     }
+
+
+// };
+
+var stories = {};
+
+
+const storyNames= ["Growing Mommy's Dick", "Mr. Wolf", "Daddy I'm Scawwed", "Game Night", "Emma", "Nungerie", "God Bless Estrogen Pills",
+  "Protecting A Femboy", "Hammer", "The Hanging Man", 
+];
+
+const mainPageStoryButtons = {};
+
+storyNames.forEach(name => {
+  mainPageStoryButtons[name] = {};
+
+
+  mainPageStoryButtons[name].newStoryIcon = {};
+
+  mainPageStoryButtons[name].lockIcons ={};
+  mainPageStoryButtons[name].hiddenIcons ={};
+  mainPageStoryButtons[name].notLikedIcons ={};
+  mainPageStoryButtons[name].likedIcons ={};
+  
+})
+
+const PatreonStoryNames = [];
+
+const lockedStoryNames = ['The Hanging Man', ]; 
+
+
+
+
+
+
+
+stories["Growing Mommy's Dick"]= {
+  num: 1,
+  folder: "1 Growing Mommys Dick",
+  overview: "Your mom complains from the hardships of life so you tell her to grow some balls. Next day she goes under surgery. Now it's your fault she has a dick and you have to nurture the tiny little thing until it grows into a healthy, full-fledged, throbbing cock.",
+  severity: "D",
+  subtitle: "Coming Soon",
+  background: "rgba(226.31, 13.20, 25.99, 0.37)",
+  genre: ["Transexual", "Virtual Pet"],
+  characters: ["Bailey", "Jake" ],
+  chapters: [
+       {
+          name: "Hardships Of Life",
+          num : 1,
+          wordCount: 5030,
+          isPatreon: false,
+          isComingSoon: false,
+          variants: {},
+          sections: [],
+      }, 
+
+    ]
+    
+    }
+
+
+;
+
+
+stories["Mr. Wolf"] = {
+    num: 5,
+    folder: "5 Mr Wolf",
+    overview: "Mr. Wolf meets a little girl in the woods.",
+    severity: "D",
+    subtitle: "Coming Soon",
+    genre: ["Parody", ],
+    characters: ["Mr. Wolf", "Girl in the woods" ],
+    chapters: [
+         {
+            name: "Girl",
+            num : 1,
+            wordCount: 248,
+            isPatreon: false,
+            isComingSoon: false,
+            variants: {},
+            sections: [],
+        }, 
+  
+      ]
+      }
+  
+  
+  ;
+  
+  stories["Daddy I'm Scawwed"] = {
+    num: 8,
+    folder: "8 Daddy Im Scawwed",
+    overview: "A dad-daughter conundrum.",
+    severity: "D",
+    subtitle: "Coming Soon",
+    genre: ["Incest", "Ageplay", "Mostly Text", "Open"],
+    characters: ["Joshua", "Birdie", "Teddy"],
+    chapters: [
+        {
+            name: "Pee",
+            num : 1,
+            wordCount: 1062,
+        }, 
+        {
+          name: "Ralf",
+          num: 2,
+          wordCount: 0,
+      }
+    ],
+    
+  };
+  
+  stories["Game Night"] = {
+    num: 12,
+    folder: "12 Game Night",
+    overview: "I joined Mom and Dad's stupid game for once.",
+    severity: "D",
+    subtitle: "Coming Soon",
+    genre: ["Incest",],
+    characters: [],
+    chapters: [
+        {
+            name: "Contest!",
+            num : 1,
+            wordCount: 423,
+        }, 
+    ]
+  
+  
+  };
+  
+  stories["Emma"] = {
+    num: 14,
+    folder: "14 Emma",
+    overview: "The grand entrance hall of the Hastings estate was a testament to Lady Arabella Hastings' refined tastes and the family's wealth. Tall windows with heavy velvet drapes let in slivers of the late afternoon sun, casting long shadows across the marbled floor. Arabella stood near the grand staircase, her dark eyes assessing the young woman who had just been shown in by the butler.",
+    severity: "D",
+    subtitle: "Coming Soon",
+    background: "rgba(140, 216, 161.28, 0.14)",
+    genre: ["Female Cuckoldery", ],
+    characters: ["Arabella", "Emma", "Henry" ],
+    chapters: [
+        {
+            name: "Perfume",
+            num : 1,
+            wordCount: 3598,
+        }, 
+
+        {
+          name: "Finally, something",
+          num : 2,
+          wordCount: 0,
+          subtitle: "Coming Soon",
+      }, 
+    ]
+  
+  
+  };
+  
+  stories["Nungerie"] = {
+    num: 17,
+    folder: "17 Nungerie",
+    overview: "In a desperate attempt to revive a dying religion, 25-year old Alistair Everhart has been elected the Bishop of Sancta Celestia Cathedral, the last church in the world, due to his otherworldly talent in marketing and brand strategy. His first decree was to force nuns and pastorettes into wearing more revealing clothes to attract more customers of God.",
+    severity: "D",
+    subtitle: "Coming Soon",
+    genre: ["Religion", "Corruption", "Strategy"],
+    characters: ["Alistair Everhart", "Pristine Rivers", "Father Gregory" ],
+    chapters: [
+        {
+            name: "Prologue",
+            num : 1,
+            wordCount: 942,
+        }, 
+    ]
+  
+  
+  };
+  
+  stories["God Bless Estrogen Pills"] = {
+    num: 18,
+    folder: "18 God Bless Estrogen Pills",
+    overview: "Cum... That's lots of cum. Wait. Did I just cum?!!!!",
+    severity: "D",
+    subtitle: "Coming Soon",
+    genre: ["Lustcraze", "Sissification"],
+    characters: [],
+    chapters: [
+        {
+            name: "God Bless Estrogen Pills",
+            num: 1,
+            wordCount: 191,
+        }, 
+    ]
+  
+  
+  };
+  
+  stories["Protecting A Femboy"] = {
+    num: 19,
+    folder: "19 Protecting A Femboy",
+    overview: "A short story about two boys.",
+    severity: "D",
+    subtitle: "Coming Soon",
+    background: "rgba(13.20, 149.59, 226.31, 0.14)",
+    genre: ["Exhibitionism", "Gay", "Voyeurism"],
+    characters: ["Drew", "Damian", "Jaime"],
+    chapters: [
+        {
+            name: "Jaime",
+            num: 1,
+            wordCount: 2028,
+        }, 
+    ]
+  
+  
+  };
+  
+  stories["Hammer"] = {
+    num: 20,
+    folder: "20 Hammer",
+    overview: "Overview unavailable.",
+    buttonStyle: "",
+    severity: "B",
+    NTR: "Exclusive",
+    subtitle: "Coming Soon",
+    genre: ["Agony", ],
+    characters: [],
+    chapters: [
+        {
+            name: "Hammer",
+            num: 1,
+            wordCount: 1030,
+        }, 
+  
+        {
+          name: "Sunlight",
+          num: 2,
+          wordCount: 3277,
+      }, 
+    ]
+
+  };
+
+  stories["The Hanging Man"] = {
+    num: 21,
+    folder: "21 The Hanging Man",
+    overview: "You need to read 9 stories to unlock this story.",
+    severity: "B",
+    subtitle: "Coming Soon",
+    NTR: "Exclusive",
+    genre: [],
+    characters: [],
+    chapters: [
+        {
+            name: "The Hanging Man",
+            num: 1,
+            wordCount: 0,
+            subtitle: "Coming Soon"
+        }, 
+  
+    ]
+
+  };
+
+
+function initStories() {
+  
+    storyNames.forEach(i => {
+
+      data.StoryObj[i]= new Story(i);
+    
+
+  });
+
+
+}
